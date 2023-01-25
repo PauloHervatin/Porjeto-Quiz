0280e365266235a8e6a65ccf2e33af9bd2b60073
PauloHervatin <120523159+PauloHervatin@users.noreply.github.com>
2023-01-25 14:58:25 -0300

    Complemento

--- Quiz/src/context/quiz.jsx
+++ Quiz/src/context/quiz.jsx
@@ -1,8 +1,30 @@
+import { useReducer } from "react";
 import { createContext } from "react";
+import questions from '../data/questions'
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialstate = {
+    gameStage: STAGES[0],
+    questions,
+}
+
+const quizReducer = (state, action) => {
+    console.log(state, action);
+
+    switch (action.type) {
+        case "CHANGE_STATE":
+            return state;
+
+        default: 
+            return state;
+    }
+}
 
 export const QuizContext = createContext();
 
-export const QuizProvider = ({children}) => {
-    const value = {};
+export const QuizProvider = ({ children }) => {
+    const value = useReducer(quizReducer, initialstate);
+
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
